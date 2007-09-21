88eb3bced883c0c2ec5d898f8c7722ac49d37379
Keripo <Keripo@659cf0a5-a83a-0410-bbf7-c2d6c14b6cfa>
2007-09-21 20:13:17 +0000

    Removed file/folder

--- base/zerolauncher/launch/FastLaunch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#set -vx
-#exec >> /root/Packs/ZeroLauncher/Misc/FastLaunch.log 2>&1
-
-#clear
-#cd /root/Packs/ZeroLauncher
-#exec /root/Packs/ZeroLauncher/ZeroLauncher
-# >> /root/Packs/ZeroLauncher/Misc/FastLaunch.log 2>&1
-exec /root/ZeroLauncher/Launch.sh
