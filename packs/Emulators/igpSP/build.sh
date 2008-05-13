97333a2c5ecb44421a07f8a395822e6116d93615
Philip Peng <k.darktiger@gmail.com>
2008-05-13 01:01:40 +0000

    Whoops, forgot to add igpSP's build.sh!

---
+++ packs/Emulators/igpSP/build.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+#
+# igpSP Auto-Building Script
+# Created by Keripo
+# For Project ZeroSlackr
+# Last updated: May 11, 2008
+#
+echo ""
+echo "==========================================="
+echo ""
+echo "igpSP Auto-Building Script"
+echo ""
+# Cleanup
+if [ -d build ]; then
+	echo "> Removing old build directory..."
+	rm -rf build
+fi
+# Make new compiling directory
+echo "> Setting up build directory..."
+mkdir build
+cd build
+# Copying full source
+echo "> Extracting source..."
+tar jxf ../src/orig/gpsp09-2xb_src.tar.bz2
+mv gpsp_src compiling
+# Apply ZeroSlackr custom patches
+echo "> Applying ZeroSlackr patches..."
+cd compiling
+cp -rf ../../src/mod/* ./
+for file in ../../src/patches/*; do
+	patch -p0 -t -i $file >> ../build.log
+done
+cd ..
+# Symlink the libraries
+echo "> Symlinking libraries..."
+DIR=$(pwd)
+LIBSDIR=../../../../libs
+LIBS="hotdog ttk launch"
+for lib in $LIBS
+do
+	if [ ! -d $LIBSDIR/$lib ]; then
+		cd $LIBSDIR
+		echo "  - Building "$lib"..."
+		./src/$lib.sh
+		echo ""
+		cd $DIR
+	fi
+	ln -s $LIBSDIR/$lib ./
+done
+# Compiling
+echo "> Compiling..."
+echo "  Note: All warnings/errors here will"
+echo "  be logged to the 'build.log' file."
+echo "  If building fails, check the log file."
+echo "  Note 2: Building igpSP is processor"
+echo "  intensive; if you have an old computer"
+echo "  it is recommended that you skip this."
+cd compiling
+export PATH=/usr/local/arm-uclinux-tools2/bin:/usr/local/arm-uclinux-elf-tools/bin:/usr/local/arm-uclinux-tools/bin:$PATH
+cd ipod
+echo "  - Compiling no-sound build..."
+make NOSOUND=1 ZEROSLACKR=1 >> ../../build.log 2>&1
+mv igpSP igpSP-no-sound
+rm -rf gui.o main.o memory.o igpSP.gdb
+echo "  - Compiling with-sound build..."
+make ZEROSLACKR=1 >> ../../build.log 2>&1
+mv igpSP igpSP-with-sound
+cd ..
+# Copy over compiled file
+echo "> Copying over compiled files..."
+cd ..
+mkdir compiled
+cp -rf compiling/ipod/igpSP-no-sound compiled/
+cp -rf compiling/ipod/igpSP-with-sound compiled/
+# Launch module
+echo "> Building ZeroLauncher launch module..."
+cp -rf ../src/launcher ./
+cd launcher
+export PATH=/usr/local/arm-uclinux-tools2/bin:/usr/local/arm-uclinux-elf-tools/bin:/usr/local/arm-uclinux-tools/bin:$PATH
+make -f ../launch/launch.mk
+cd ..
+# Creating release
+echo "> Creating 'release' folder..."
+cp -rf ../src/release ./
+cd release
+# Files
+PACK=ZeroSlackr/opt/Emulators/igpSP
+cp -rf ../compiled/igpSP-no-sound $PACK/
+cp -rf ../compiled/igpSP-with-sound $PACK/
+cp -rf ../compiling/game_config.txt $PACK/Data/
+cp -rf ../launcher/* $PACK/Launch/
+# Documents
+DOCS=$PACK/Misc/Docs
+cp -rf "../../ReadMe from Keripo.txt" $PACK/
+cp -rf ../../License.txt $PACK/
+cp -rf ../../src/patches $PACK/Misc/Patches
+FILES="COPYING.DOC readme.txt gp2x/readme_gp2x.txt"
+for file in $FILES
+do
+	cp -rf ../compiling/$file $DOCS/
+done
+sh -c "find -name '.svn' -exec rm -rf {} \;" >> /dev/null 2>&1
+# Archive documents
+cd $PACK/Misc
+tar -cf Patches.tar Patches
+gzip --best Patches.tar
+rm -rf Patches
+tar -cf Docs.tar Docs
+gzip --best Docs.tar
+rm -rf Docs
+# Done
+echo ""
+echo "Fin!"
+echo ""
+echo "Auto-Building script by Keripo"
+echo ""
+echo "==========================================="
