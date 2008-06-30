34eb46663ded6375feaeb33f90e88b33ce4ddf2a
Keripo <Keripo@659cf0a5-a83a-0410-bbf7-c2d6c14b6cfa>
2008-06-30 21:00:21 +0000

    Added TinySID; corrected LOLCODE interpreter info.

---
+++ packs/Media/TinySID/build.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+#
+# TinySID Auto-Building Script
+# Created by Keripo
+# For Project ZeroSlackr
+# Last updated: Jun 29, 2008
+#
+echo ""
+echo "==========================================="
+echo ""
+echo "TinySID Auto-Building Script"
+echo ""
+# Cleanup
+if [ -d build ]; then
+	echo "> Removing old build directory..."
+	rm -rf build
+fi
+# Make new compiling directory
+echo "> Setting up build directory..."
+mkdir build
+cd build
+# Extract source
+echo "> Extracting source..."
+mkdir compiling
+cd compiling
+tar zxf ../../src/orig/tinysid-linux.tar.gz
+cd ..
+# Apply ZeroSlackr custom patches
+echo "> Applying ZeroSlackr patches..."
+cp -rf ../src/mod/* compiling/
+cd compiling
+for file in ../../src/patches/*; do
+	patch -p0 -t -i $file >> ../build.log
+done
+cd ..
+# Symlink the libraries
+echo "> Symlinking libraries..."
+DIR=$(pwd)
+LIBSDIR=../../../../libs
+LIBS="ttk launch"
+for lib in $LIBS
+do
+	if [ ! -d $LIBSDIR/$lib ]; then
+		cd $LIBSDIR
+		echo "  - Building "$lib"..."
+		./src/$lib.sh
+		echo ""
+		cd $DIR
+	fi
+	ln -s $LIBSDIR/$lib ./
+done
+# Compiling
+echo "> Compiling..."
+cd compiling
+export PATH=/usr/local/arm-uclinux-tools2/bin:/usr/local/arm-uclinux-elf-tools/bin:/usr/local/arm-uclinux-tools/bin:$PATH
+make >> ../build.log
+# Copy over compiled file
+echo "> Copying over compiled files..."
+cd ..
+mkdir compiled
+cp -rf compiling/tinysid compiled/TinySID
+# Launch module
+echo "> Building ZeroLauncher launch module..."
+cp -rf ../src/launcher ./
+cd launcher
+export PATH=/usr/local/arm-uclinux-tools2/bin:/usr/local/arm-uclinux-elf-tools/bin:/usr/local/arm-uclinux-tools/bin:$PATH
+make -f ../launch/launch.mk
+cd ..
+# Creating release
+echo "> Creating 'release' folder..."
+cp -rf ../src/release ./
+cd release
+# Files
+PACK=ZeroSlackr/opt/Media/TinySID
+cp -rf ../compiled/TinySID $PACK/
+cp -rf ../compiling/Freeze.sid $PACK/SIDs/
+cp -rf ../launcher/* $PACK/Launch/
+# Documents
+DOCS=$PACK/Misc/Docs
+cp -rf "../../ReadMe from Keripo.txt" $PACK/
+cp -rf ../../License.txt $PACK/
+cp -rf ../../src/patches $PACK/Misc/Patches
+cp -rf ../../src/mod $PACK/Misc/Mod
+# Archive documents
+cd $PACK/Misc
+tar -cf Patches.tar Patches
+gzip --best Patches.tar
+rm -rf Patches
+tar -cf Mod.tar Mod
+gzip --best Mod.tar
+rm -rf Mod
+tar -cf Docs.tar Docs
+gzip --best Docs.tar
+rm -rf Docs
+# Done
+echo ""
+echo "Fin!"
+echo ""
+echo "Auto-Building script by Keripo"
+echo ""
+echo "==========================================="
