ffcfa1b8e347fe63dd76e8dd3fbf4e656461c7ad
Philip Peng <k.darktiger@gmail.com>
2008-10-13 22:33:24 +0000

    PZ2 patch fixes and updated Flite launch module.

--- base/ZeroLauncher/src/mod/add-ons/ZeroLauncher/ZeroLauncher.c
+++ base/ZeroLauncher/src/mod/add-ons/ZeroLauncher/ZeroLauncher.c
@@ -1,5 +1,5 @@
 /*
- * Last updated: July 26, 2008
+ * Last updated: Oct 12, 2008
  * ~Keripo
  *
  * Copyright (C) 2008 Keripo
@@ -32,7 +32,7 @@ static PzWindow *fastlaunch()
 
 static PzWindow *readme()
 {
-	return new_textview_window(
+	return pz_new_textview_window(
 		"/opt/Base/ZeroLauncher/ReadMe from Keripo.txt");
 }
 
